d60ed1520ce1e5953de497db11b2b3684b8c4ed0
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-09 14:23:43 +0100

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -1,5 +1,5 @@
 def pattern(value):
-    patterns = ['<p>', '</p>','<b>','</b>', '<br/>','<i>','</i>','<br>']
+    patterns = ['<p>', '</p>','<b>','</b>', '<br/>','<i>','</i>','<br>','<a href="/abudawud/letter">','</a>']
 
     for i in patterns:
         if i in value:
